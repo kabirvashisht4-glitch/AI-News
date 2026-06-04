4898ae8c89303d5363fae90fc9a1405c308fc556
kabirvashisht4-glitch <kabirvashisht4@gmail.com>
2026-06-04 21:36:30 +0530

    some changes

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,18 +1,30 @@
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
+import { LineChart, Line, XAxis, YAxis } from 'recharts'
 export default function Body() {
+    const data = [
+        { time: 1, value: 100 },
+        { time: 2, value: 120 },
+        { time: 3, value: 115 },
+        ]
+    const [d, setData] = useState("")
+    const y = (a) => {
+        setData(a)
+    }
+
     return (
         <main id="arena">
-            <div className="sector1"></div>
+            
+            <div className="sector1">{d}
+            </div>
             <div className="sectorx">
-                <button className="sector2">Chat GPT</button>
-                <button className="sector2">Gemini</button>
-                <button className="sector2">Claude</button>
-                <button className="sector2">Kimi</button>
-                <button className="sector2">Deepseek</button>
-                <button className="sector2">Llama</button>
-                <button className="sector2">Grok</button>
-                <button className="sector2">Phi</button>
+                <button className="sector2" onClick={() => y("Chat GPT")}>Chat GPT</button>
+                <button className="sector2" onClick={() => y("Gemini")}>Gemini</button>
+                <button className="sector2" onClick={() => y("Claude")}>Claude</button>
+                <button className="sector2" onClick={() => y("Kimi")}>Kimi</button>
+                <button className="sector2" onClick={() => y("Deepseek")}>Deepseek</button>
+                <button className="sector2" onClick={() => y("Llama")}>Llama</button>
+                <button className="sector2" onClick={() => y("Grok")}>Grok</button>
+                <button className="sector2" onClick={() => y("Phi")}>Phi</button>
             </div>
         </main>
     )
