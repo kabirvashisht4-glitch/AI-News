e1b6f2307d3b5650d30eaca2139159559e375caa
kabirvashisht4-glitch <kabirvashisht4@gmail.com>
2026-06-08 21:58:33 +0530

    Some more Changes

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,31 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { LineChart, Line, XAxis, YAxis } from 'recharts'
-export default function Body() {
-    const data = [
-        { time: 1, value: 100 },
-        { time: 2, value: 120 },
-        { time: 3, value: 115 },
-        ]
-    const [d, setData] = useState("")
-    const y = (a) => {
-        setData(a)
-    }
-
+export default function Body(props) {
+    let {work} = props
+    
     return (
         <main id="arena">
-            
-            <div className="sector1">{d}
-            </div>
-            <div className="sectorx">
-                <button className="sector2" onClick={() => y("Chat GPT")}>Chat GPT</button>
-                <button className="sector2" onClick={() => y("Gemini")}>Gemini</button>
-                <button className="sector2" onClick={() => y("Claude")}>Claude</button>
-                <button className="sector2" onClick={() => y("Kimi")}>Kimi</button>
-                <button className="sector2" onClick={() => y("Deepseek")}>Deepseek</button>
-                <button className="sector2" onClick={() => y("Llama")}>Llama</button>
-                <button className="sector2" onClick={() => y("Grok")}>Grok</button>
-                <button className="sector2" onClick={() => y("Phi")}>Phi</button>
-            </div>
+            <div className="sector1">{work}</div>
         </main>
     )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,9 +1,20 @@
 import React, {useState, useEffect} from "react";
-
+import Body from "./Body";
 export default function Sidebar() {
+    const [bat , setBat] = useState("")
+    const y = (bat) => {
+        setBat(bat)
+    }
     return (
         <nav id="trainer1">
-            <h2>NEWS</h2>
+            <body work={bat}/>
+            <div className="trainer2">
+                <button onClick={() => {y}}>{"Space"}</button>
+                <button onClick={() => {y}}>{"Robotics"}</button>
+                <button onClick={() => {y}}>{"DevOps"}</button>
+                <button onClick={() => {y}}>{"Cyber Security"}</button>
+                <button onClick={() => {y}}>{"Biology"}</button>
+            </div>
         </nav>
     )
 }
